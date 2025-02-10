724b3da73c69eff89d2368e54b492936c234bec8
Srinithish SM <157304330+Srinithish-07@users.noreply.github.com>
2025-02-10 21:05:12 +0530

    Create day1.py

---
+++ Beginner Projects/day1.py
@@ -0,0 +1,26 @@
+# DAY 1
+# Topics Learned
+
+    # print() function
+    # input() function
+    # variables
+    # String manipulation 
+    # Debugging
+    # Commenting
+
+
+##### BAND NAME GENERATOR....
+
+print()
+# Create a greeting for your program.
+print("Welcome to the Band Name Generator...")
+
+# Ask for the city name your user grew up.
+city = input("What's the name of the city you grew up in?\n")
+
+# Ask the name of their pet.
+pet = input("What's your pet's name?\n")
+
+# Combine the city and pet name and show them their band name.
+print("Your band name could be "+ city + " " + pet)
+print()
